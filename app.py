22fa915015fb6187936d7fce096a6b605b8612cb
AntonSangho <sanghoemail@gmail.com>
2021-10-17 19:42:18 +0900

    .

--- app.py
+++ app.py
@@ -3,8 +3,3 @@ from main import app
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 9999, debug = True)
-# app = Flask(__name__)
-
-# @app.route("/")
-# def home():
-#     return "Hello, Flask"
