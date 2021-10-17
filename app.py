ec5c6e0373f014aed4c1e88a4042a3b51ebed0b1
AntonSangho <sanghoemail@gmail.com>
2021-10-17 18:30:49 +0900

    blueprint example

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+from main import app
 
-@app.route("/")
-def home():
-    return "Hello, Flask"
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 9999, debug = True)
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Hello, Flask"
